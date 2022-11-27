e9cb4d818b2fa4fab680d9cffa9c4ddcc1be5b57
Vladimir <hotsru12@gmail.com>
2022-11-27 23:17:48 +0300

    ready

---
+++ components/Loading.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, ActivityIndicator, Text} from 'react-native';
+const Loading = () => {
+	return (
+		<View style={{
+			flex: 1,
+			justifyContent: 'center',
+			alignItems: 'center'
+		 }}>
+			<ActivityIndicator size='large'/>
+			<Text style={{
+				fontSize: 16,
+				marginTop: 5
+			}}>Загрузка...</Text>
+		 </View>
+	)
+}
+
+export default Loading
---
+++ screens/FullPost.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import styled from 'styled-components/native';
+import axios from 'axios'
+import Loading from '../components/Loading';
+const FullPostView = styled.View`
+	padding: 15px;
+`
+const PostImage = styled.Image`
+	border-radius: 10px;
+	width: 100%;
+	height: 250px;
+	margin-bottom: 20px;
+`
+
+const PostText = styled.Text`
+	font-size: 18px;
+	line-height: 24px;
+`
+
+const FullPostScreen = ({route, navigation}) => {
+
+	const [article, setArticle] = React.useState('');
+	const [loading, setLoading] = React.useState(true)
+	const {id, title} = route.params;
+
+	
+
+	React.useEffect(() => {
+		navigation.setOptions({
+			title
+		})
+		setLoading(true)
+		axios.get(`https://632009aae3bdd81d8ef10d61.mockapi.io/articles/${id}`).then(({data}) => {
+			setArticle(data)
+		}).catch(err => {
+			console.log(err);
+			Alert.alert('Ошибка','Ошибка при подключении cтатью')
+		}).finally(() => {
+			setLoading(false)
+		})
+	}, [])
+	if(loading) {
+		return (
+			<Loading/>
+		)
+	  }
+	return (
+		<FullPostView>
+			<PostImage source={{uri: article.imageUrl}}/>
+			<PostText>{article.text}</PostText>
+		</FullPostView>
+	)
+}
+
+export default FullPostScreen
